4a35270d8cb0f9960bc0e580d3125b91342b1059
Timothy Lombard <timothylombard@gmail.com>
2020-12-17 20:30:04 +1100

    removed dummy text widget replaced with on getExchange widget

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -172,28 +172,7 @@ class _PriceScreenState extends State<PriceScreen> {
               ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  //15. Update the Text Widget with the data in bitcoinValueInUSD.
-                  'Convert ${cryptoDescription[selectCrypto]} to ${currencyDescription[selectFiat]}',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
+
           GetExchangeWidget(
             horizontalInset: 20,
             onPressed: () => getData(targetURL),
